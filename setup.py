045034a1db82da6b53401a3e315843c41b0db1f1
Juda Kaleta <juda.kaleta@gmail.com>
2012-10-05 16:57:18 +0200

    Add templatetags to setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     author='Juda Kaleta',
     author_email='juda.kaleta@gmail.com',
-    packages=['cstypo', 'cstypo.tests'],
+    packages=['cstypo', 'cstypo.templatetags', 'cstypo.tests'],
     scripts=['bin/cstypo.py'],
     url='https://github.com/yetty/cstypo',
     license=open('LICENSE').read(),
